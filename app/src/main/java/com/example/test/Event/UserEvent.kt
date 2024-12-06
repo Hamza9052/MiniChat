e4907a0f8ce23587803d562caba044f0e1661ce1
Hamza <hamzh9052@gmail.com>
2024-12-07 00:01:33 +0100

    update

--- app/src/main/java/com/example/test/Event/UserEvent.kt
+++ app/src/main/java/com/example/test/Event/UserEvent.kt
@@ -4,7 +4,7 @@ import android.content.Context
 
 
 interface UserEvent {
-    data class Login(val user: user,val state:(state:Boolean)->Unit) : UserEvent
+    data class Login(val email: String,val password: String,val state:(state:Boolean)->Unit) : UserEvent
     data class CreateAccount(val user: user,val state:(state:Boolean)->Unit) : UserEvent
     data class signOut(val state:(state:Boolean)->Unit) : UserEvent
     data class Upload_Image(var image:String) : UserEvent
